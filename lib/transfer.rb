c0c5a2114d7687763de48c8854b4eefafcb98992
Veronica Kim <vero.a.kim@gmail.com>
2018-03-02 02:53:19 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,24 +9,24 @@ attr_accessor :transfer, :sender, :status, :receiver, :amount
     @receiver = receiver
   end
 
-  # def valid?
-  #   if receiver.valid? && sender.valid?
-  #     return true
-  #   end
-  # end
-  #
-  # def execute_transaction
-  #   if self.status == "pending"
-  #     if sender.valid? && sender.balance > transfer
-  #         sender.balance = sender.balance - @amount
-  #         receiver.balance = receiver.balance + @amount
-  #         self.status = "complete"
-  #       elsif self.valid? == false
-  #          self.status = "rejected"
-  #          puts "Transaction rejected. Please check your account balance."
-  #       end
-  #     end
-  # end
+  def valid?
+    if receiver.valid? && sender.valid?
+      return true
+    end
+  end
+
+  def execute_transaction
+    if self.status == "pending"
+      if sender.valid? && sender.balance > transfer
+          sender.balance = sender.balance - @amount
+          receiver.balance = receiver.balance + @amount
+          self.status = "complete"
+        elsif self.valid? == false
+           self.status = "rejected"
+           puts "Transaction rejected. Please check your account balance."
+        end
+      end
+  end
 
 
 
