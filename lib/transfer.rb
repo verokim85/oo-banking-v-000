55fd0277b9cead151655500c66af9126724622a7
Veronica Kim <vero.a.kim@gmail.com>
2018-03-02 03:19:25 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,8 +30,8 @@ attr_accessor :transfer, :sender, :status, :receiver, :amount
 
   def reverse_transfer
       if self.status == "completed"
-        receiver.balance = balance + @amount
-        sender.balance = balance + @amount
+        receiver.balance = balance 
+        sender.balance = balance 
         self.status = "reversed"
     end
   end
