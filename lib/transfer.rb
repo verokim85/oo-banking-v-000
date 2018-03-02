4d6080b8b447b9a2b489ff5d81ce99a1ce3552cb
Veronica Kim <vero.a.kim@gmail.com>
2018-03-02 02:26:54 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,10 +25,10 @@ attr_accessor :transfer, :sender, :status, :receiver, :amount
         if self.valid?
           sender.balance = sender.balance - @amount
           receiver.balance = receiver.balance + @amount
-             self.status = "complete"
+          self.status = "complete"
         elsif self.valid? == false
            return "Transaction rejected. Please check your account balance."
-              end
+        end
       end
   end
 
